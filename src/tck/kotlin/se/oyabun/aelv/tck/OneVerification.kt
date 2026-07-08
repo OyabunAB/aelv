b14057adb389ac0fc00b2309c48c5b8d355613f6
Daniel Sundberg <daniel.sundberg@oyabun.se>
2026-07-09 00:52:11 +0200

    feat: TCK compliance for One<T> using SkipException for n>1
    Closes #24

--- src/tck/kotlin/se/oyabun/aelv/tck/OneVerification.kt
+++ src/tck/kotlin/se/oyabun/aelv/tck/OneVerification.kt
@@ -3,6 +3,7 @@ package se.oyabun.aelv.tck
 import org.reactivestreams.Publisher
 import org.reactivestreams.tck.PublisherVerification
 import org.reactivestreams.tck.TestEnvironment
+import org.testng.SkipException
 import org.testng.annotations.Test
 import se.oyabun.aelv.One
 import se.oyabun.aelv.UpstreamErrorException
@@ -10,9 +11,10 @@ import se.oyabun.aelv.UpstreamErrorException
 @Test
 class OneVerification : PublisherVerification<Int>(TestEnvironment()) {
 
-    override fun createPublisher(elements: Long): Publisher<Int>? = when (elements) {
+    override fun createPublisher(elements: Long): Publisher<Int> = when (elements) {
+        0L -> One.error(UpstreamErrorException(RuntimeException("empty")))
         1L -> One.of(1)
-        else -> null
+        else -> throw SkipException("One<T> emits exactly 1 element, cannot produce $elements")
     }
 
     override fun createFailedPublisher(): Publisher<Int> = Publisher { subscriber ->
